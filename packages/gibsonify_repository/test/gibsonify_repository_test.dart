9fb2c931b8f3d1841591a08637d1656af958c06d
Adam Sroka <45038919+adam-sroka@users.noreply.github.com>
2022-02-01 18:17:03 +0000

    test(gibsonify_repository): add demo test

--- packages/gibsonify_repository/test/gibsonify_repository_test.dart
+++ packages/gibsonify_repository/test/gibsonify_repository_test.dart
@@ -1,17 +1,17 @@
 // import 'package:gibsonify_repository/gibsonify_repository.dart';
-// import 'package:test/test.dart';
+import 'package:test/test.dart';
 
 // TODO: write tests
-// void main() {
-//   group('A group of tests', () {
-//     final awesome = Awesome();
+void main() {
+  group('A group of tests', () {
+    final awesome = true;
 
-//     setUp(() {
-//       // Additional setup goes here.
-//     });
+    setUp(() {
+      // Additional setup goes here.
+    });
 
-//     test('First Test', () {
-//       expect(awesome.isAwesome, isTrue);
-//     });
-//   });
-// }
+    test('First Test', () {
+      expect(awesome, isTrue);
+    });
+  });
+}
